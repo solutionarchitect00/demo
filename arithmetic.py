2f58ea0b11e1b06a244a0d609e9f3a68ed2bbf3b
Ismail <95559246+solutionarchitect00@users.noreply.github.com>
2022-02-15 16:29:53 +0530

    this is the commit done for the python program

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+a = input(f"Enter the first number:\n")
+print(a)
+b = input(f"Enter the second number:\n")
+print(b)
+num1 = int(a)
+num2 = int(b)
+add = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mul = num1 * num2
+mod = num1 % num2
+print(f" Addition is :{add}\n")
+print(f" Substract is :{sub}\n")
+print(f" Division is :{div}\n")
+print(f" Multiplication is :{mul}\n")
+print(f" Modulus is :{mod}\n")
